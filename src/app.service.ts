37d6c250d5bb03e1c403f3d591fd1620fd08f2ab
Gianluca Nativo <giannativo@yahoo.com.ar>
2023-09-06 15:29:26 -0300

    Adding findGrammarSolutions implementation

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { Engine } from "json-rules-engine";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private engine: Engine;
+
+  constructor() {
+    this.engine = new Engine();
+    this.initializeRules();
+  }
+
+  private initializeRules() {
+    const adverbRule = {
+      conditions: {
+        any: [
+          {
+            fact: "word",
+            path: '$.*',
+            operator: "wordEndsWith",
+            value: "ly"
+          }
+        ]
+      },
+      event: {
+        type: "adverb_error",
+        params: {
+          message: "Consider eliminating the adverb for a more direct sentence."
+        },
+      }
+    };
+    this.engine.addRule(adverbRule);
+    this.engine.addOperator('wordEndsWith', (factValue: string[], jsonValue: string) => {
+      const pattern = new RegExp(`${jsonValue}$`);
+      const results = factValue.map((word) => pattern.test(word));
+      return results[0];
+    })
+  }
+
+  async findGrammarSuggestions(text: string): Promise<any[]> {
+    let suggestions = [];
+    const facts = { word: text.split(/\s+/) };
+    try {
+      const results = await this.engine.run(facts);
+      suggestions = results.events.filter(event => event.type === "adverb_error")
+      .map(event => event.params.message);
+    }
+    catch (err) {
+      console.error(err);
+      suggestions = [];
+    }
+    return suggestions;
   }
 }
